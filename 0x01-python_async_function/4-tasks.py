081308c5f640c58f6d62f2e877dc9d041e569f8c
danicelistobon <danielct-97@hotmail.com>
2021-03-17 01:07:19 +0000

    4. Tasks

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Task wait n
+"""
+import asyncio
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Take the code from wait_n and alter it into a new function task_wait_n
+        The code is nearly identical to wait_n except task_wait_random is
+        being called
+    """
+    delay = []
+
+    for i in range(n):
+        delay.append(task_wait_random(max_delay))
+
+    return [await j for j in asyncio.as_completed(delay)]
