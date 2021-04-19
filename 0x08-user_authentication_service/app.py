3245d6e07aa556c633a8a13ecaa3d3eb61759fec
danicelistobon <danielct-97@hotmail.com>
2021-04-19 03:53:16 +0000

    7. Register user

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -13,7 +13,20 @@ def basicApp() -> str:
     """ Has a single GET route ("/") and use flask.jsonify to return a
         JSON payload
     """
-    return jsonify({"message": "Bienvenue"})
+    return jsonify({"message": "Bienvenue"}), 200
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """ Implements the POST /users route
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"}), 200
+    except Exception:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
