ca9a04c62ba197e886529c851355723546d911d4
danicelistobon <danielct-97@hotmail.com>
2021-04-19 05:01:38 +0000

    11. Log in

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -28,6 +28,19 @@ def users() -> str:
     except Exception:
         return jsonify({"message": "email already registered"}), 400
 
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """ Create a new session for the user, store it the session ID as a cookie
+        with key session_id
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    session_id = AUTH.create_session(email)
+    res = jsonify({"email": email, "message": "logged in"})
+    res.set_cookie('session_id', session_id)
+    return res
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
