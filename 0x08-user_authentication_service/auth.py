6eb5eba6f540f9a6c6dc93666f9d34b7071f074f
danicelistobon <danielct-97@hotmail.com>
2021-04-19 04:22:09 +0000

    10. Get session ID

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -48,3 +48,14 @@ class Auth:
                 return True
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """ Takes an email string argument and returns the session ID as a str
+        """
+        session_id = _generate_uuid()
+        try:
+            user = self._db.find_user_by(email=email)
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
