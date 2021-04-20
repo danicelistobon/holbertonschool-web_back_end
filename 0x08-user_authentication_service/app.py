3677da0f0a7b9c2af2c0d151c93c3ae5b9240aed
danicelistobon <danielct-97@hotmail.com>
2021-04-20 04:45:51 +0000

    19. Update password end-point

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -85,5 +85,21 @@ def get_reset_password_token() -> str:
         return jsonify({"email": email, "reset_token": reset_token}), 200
 
 
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password() -> str:
+    """ Implement a function to respond to the PUT /reset_password route. The
+        request is expected to contain form data with fields "email",
+        "reset_token" and "new_password"
+    """
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
+    try:
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"}), 200
+    except Exception:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
