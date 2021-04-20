e7d7a2e89db370444cea417dc91dfc54f2d269ec
danicelistobon <danielct-97@hotmail.com>
2021-04-20 04:07:28 +0000

    15. User profile

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -58,5 +58,19 @@ def logout():
     return redirect('/')
 
 
+@app.route('/profile', methods=['GET'], strict_slashes=False)
+def profile() -> str:
+    """ Implement a profile function to respond to the GET /profile route. The
+        request is expected to contain a session_id cookie. Use it to find the
+        user. If the user exist, respond with a 200 HTTP status
+    """
+    session_id = request.cookies.get('session_id')
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
+        return jsonify({"email": user.email}), 200
+    else:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
