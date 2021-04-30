48a74c42fb7e5c9ae1c9753011c39d9f790d0771
Daniel Celis <danielct-97@hotmail.com>
2021-04-30 04:23:38 +0000

    5. Email validation to sent

---
+++ 0x0C-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,12 @@
+-- Email validation to sent
+-- Creates a trigger that resets the attribute valid_email
+DELIMITER |
+CREATE TRIGGER email_bool
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email <> OLD.email THEN
+    SET NEW.valid_email = 0;
+    END IF;
+END;
+|
