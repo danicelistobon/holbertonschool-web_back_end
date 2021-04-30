7dde64eac612f6638d4c28c351876314ad11aed9
Daniel Celis <danielct-97@hotmail.com>
2021-04-30 04:06:51 +0000

    0. We are all unique!

---
+++ 0x0C-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- We are all unique!
+-- Creates a table users
+CREATE TABLE If NOT EXISTS `users` (  
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `email` VARCHAR(255) NOT NULL UNIQUE,
+    `name` VARCHAR(255)
+);
